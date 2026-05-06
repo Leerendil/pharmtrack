eacc6b8d7bd4a06934a84bf7f3a2fe1db0980a42
Toyota <toyota@Toyotas-MacBook-Pro.local>
2026-05-06 15:12:26 +0300

    feat: add OrderController

--- order-service/src/main/java/org/vsu/orderservice/controller/OrderController.java
+++ order-service/src/main/java/org/vsu/orderservice/controller/OrderController.java
@@ -1,8 +1,16 @@
 package org.vsu.orderservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.vsu.orderservice.dto.OrderResponse;
+import org.vsu.orderservice.dto.OrderStatusUpdate;
 import org.vsu.orderservice.service.OrderService;
 
 @RestController
@@ -10,4 +18,14 @@ import org.vsu.orderservice.service.OrderService;
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> create(@AuthenticationPrincipal Jwt jwt) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.create(jwt));
+    }
+
+    @PatchMapping
+    public ResponseEntity<OrderResponse> changeOrderStatus(OrderStatusUpdate updateDto) {
+        return ResponseEntity.ok(orderService.changeOrderStatus(updateDto));
+    }
 }
