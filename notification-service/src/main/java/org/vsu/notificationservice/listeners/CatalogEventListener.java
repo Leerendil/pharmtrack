56257f3691a2750e639e589583f8b2581ab8e06e
Toyota <toyota@192.168.1.96>
2026-05-11 16:06:58 +0300

    feat: add application notification systme

--- notification-service/src/main/java/org/vsu/notificationservice/listeners/CatalogEventListener.java
+++ notification-service/src/main/java/org/vsu/notificationservice/listeners/CatalogEventListener.java
@@ -1,4 +1,43 @@
 package org.vsu.notificationservice.listeners;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import org.vsu.notificationservice.dto.ApplicationEvent;
+import org.vsu.notificationservice.service.NotificationService;
+import org.vsu.notificationservice.utils.exception.KafkaMalfunctionException;
+
+@Component
+@RequiredArgsConstructor
 public class CatalogEventListener {
+    private final NotificationService notificationService;
+    private final ObjectMapper objectMapper;
+
+    @Value("${admin-mail}")
+    private String adminMail;
+
+    @KafkaListener(topics = "${application-topic}", groupId = "${spring.kafka.consumer.group-id}")
+    public void collectMessage(String message) {
+        try {
+            ApplicationEvent event = objectMapper.readValue(message, ApplicationEvent.class);
+
+            String subject = "Pharmtrack admin-notification. New Manufacturer Status Request.";
+            String text = """
+                    Hello, a new application for MANUFACTURER status has been submitted.\n
+                    User Details:
+                    \n\tUser ID:"""+event.getApplicantId()+"""
+                    \n\tCompany name: """+event.getName()+"""
+                    \n\tCompany email: """+event.getCompanyMail()+"""
+                    \n\tCountry: """+event.getCountry()+"""
+                    Please review the application and take action.
+                    """;
+
+            notificationService.sendMessage(adminMail, subject, text);
+
+        } catch (Exception e) {
+            throw new KafkaMalfunctionException(e);
+        }
+    }
 }
