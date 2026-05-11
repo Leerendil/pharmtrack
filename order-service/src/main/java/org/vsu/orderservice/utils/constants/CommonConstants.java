ee3cfd6ffd59f4212e0eca7ea69922d97698546c
Toyota <toyota@192.168.1.96>
2026-05-11 16:48:16 +0300

    feat: add gateway-service base impl

--- order-service/src/main/java/org/vsu/orderservice/utils/constants/CommonConstants.java
+++ order-service/src/main/java/org/vsu/orderservice/utils/constants/CommonConstants.java
@@ -5,8 +5,8 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class CommonConstants {
     public static final String API_V1_ORDERS = "/api/v1/orders";
-
     public static final String API_V1_CARTS = "/api/v1/carts";
+
     public static final String SELECTED = "/selected";
     public static final String MEDICINE_ID = "/{medicineId}";
 
